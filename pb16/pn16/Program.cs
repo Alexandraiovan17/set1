3b4636cbf8c1db786135ee6e175c93cca7afcbd0
Alexandraiovan17 <116071293+Alexandraiovan17@users.noreply.github.com>
2024-01-19 23:10:07 +0200

    Add files via upload

---
+++ pb16/pn16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pb16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = int.Parse(Console.ReadLine());
+            double b = int.Parse(Console.ReadLine());
+            double c = int.Parse(Console.ReadLine());
+            double d = int.Parse(Console.ReadLine());
+            double e = int.Parse(Console.ReadLine());
+            SortareCrescatoare(ref a, ref b);
+            SortareCrescatoare(ref a, ref c);
+            SortareCrescatoare(ref a, ref d);
+            SortareCrescatoare(ref a, ref e);
+            SortareCrescatoare(ref b, ref c);
+            SortareCrescatoare(ref b, ref d);
+            SortareCrescatoare(ref b, ref e);
+            SortareCrescatoare(ref c, ref d);
+            SortareCrescatoare(ref c, ref e);
+            SortareCrescatoare(ref d, ref e);
+
+            Console.WriteLine($"{a}, {b}, {c}, {d}, {e}");
+        }
+        static void SortareCrescatoare(ref double x, ref double y)
+        {
+            if (x > y)
+            {
+                double aux = x;
+                x = y;
+                y = aux;
+            }
+        }
+    }
+}
---
+++ pb6/pb6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pb6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a < b + c && b < a + c && c < a + b)
+            {
+                Console.WriteLine("DA");
+            }
+            else
+                Console.WriteLine("NU");
+        }
+    }
+}
