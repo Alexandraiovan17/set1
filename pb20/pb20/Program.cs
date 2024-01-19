7fd4711e204b399adb0d6bb774b7799d467771f7
Alexandraiovan17 <116071293+Alexandraiovan17@users.noreply.github.com>
2024-01-19 23:12:43 +0200

    Add files via upload

---
+++ pb20/pb20/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceți numărătorul m si numitorul n: ");
+        int m = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        string rasp = TransfInZec(m, n);
+        Console.WriteLine($"Rezultatul este: {rasp}");
+    }
+
+    static string TransfInZec(int numarator, int numitor)
+    {
+        int parteIntreaga = numarator / numitor;
+        int rest = numarator % numitor;
+        string parteZecimala = "";
+        string perioada = "";
+
+        if (FaraPerioada(numitor))
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                rest *= 10;
+                parteZecimala += (rest / numitor).ToString();
+                rest %= numitor;
+            }
+            return $"{parteIntreaga}.{parteZecimala}";
+        }
+        else
+        {
+            while (rest != 0)
+            {
+                rest *= 10;
+                parteZecimala += (rest / numitor).ToString();
+                rest %= numitor;
+
+                int index = parteZecimala.IndexOf((rest % numitor).ToString());
+                if (index != -1)
+                {
+                    perioada = parteZecimala.Substring(index);
+                    parteZecimala = parteZecimala.Substring(0, index);
+                    break;
+                }
+            }
+            return $"{parteIntreaga}.{parteZecimala}({perioada})";
+        }
+    }
+
+    static bool FaraPerioada(int numitor)
+    {
+        while (numitor % 2 == 0 || numitor % 5 == 0)
+        {
+            if (numitor % 2 == 0) numitor /= 2;
+            if (numitor % 5 == 0) numitor /= 5;
+        }
+        return numitor == 1;
+    }
+}
---
+++ pb21/pb21/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pb21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alegeti un numar intre 1 si 1024. Raspundeti cu 'da' sau 'nu'. "); 
+
+            int min = 1 , max=1024, incercare =0;
+
+            while(true)
+                {
+                if (min == max)
+                {
+                    incercare = min;
+                    break;
+                }
+                if ((max - min) % 2 == 0)
+                {
+                    incercare = (min + max) / 2;
+                }
+                else
+                {
+                    incercare = (min + max+ 1) / 2;
+                }
+
+               
+
+                Console.Write($"Numărul dumneavoastră este mai mare sau egal decât {incercare}? (da/nu): ");
+                string raspuns = Console.ReadLine();
+                if (raspuns.Equals("da",StringComparison.OrdinalIgnoreCase))
+                {
+                    min = incercare;
+                }
+                if(raspuns.Equals("nu",StringComparison.OrdinalIgnoreCase))
+                {
+                    max = incercare - 1;
+                }
+              }
+            Console.WriteLine($"Numărul dumneavoastră este {incercare}!");
+        }
+    }
+}
